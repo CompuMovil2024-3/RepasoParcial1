a8d35b8cc656784d75cd9907807d8ead3720307e
TatisVivas <t_vivas@javeriana.edu.co>
2024-09-14 18:57:29 -0500

    actividad de números
    Porfa cielo, en vez de que el intent inicie "teAmoActivity" que sea la que tu crees

--- app/src/main/java/com/example/samplecmp/MainActivity.kt
+++ app/src/main/java/com/example/samplecmp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnShowDialog.setOnClickListener {
-            startActivity(Intent(this, TeAmoActivity::class.java))
+            startActivity(Intent(this, NumerosActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/samplecmp/NumerosActivity.kt
+++ app/src/main/java/com/example/samplecmp/NumerosActivity.kt
@@ -1,20 +1,29 @@
 package com.example.samplecmp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.samplecmp.databinding.ActivityNumerosBinding
 
 class NumerosActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNumerosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_numeros)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityNumerosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.enviar.setOnClickListener{
+            val intent = Intent(this, TeAmoActivity::class.java)
+            var numero1 = binding.numero1.text.toString()
+            var numero2 = binding.numero2.text.toString()
+            var numero3= binding.numero3.text.toString()
+            intent.putExtra("numero1", numero1)
+            intent.putExtra("numero2", numero2)
+            intent.putExtra("numero3", numero3)
+            startActivity(intent)
         }
     }
 }
