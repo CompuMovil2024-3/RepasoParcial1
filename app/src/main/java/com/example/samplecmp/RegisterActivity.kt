fa83b5c154d858175e598b6390a5861349cd7344
TatisVivas <t_vivas@javeriana.edu.co>
2024-09-15 16:03:37 -0500

    clase usuario

--- app/src/main/java/com/example/samplecmp/RegisterActivity.kt
+++ app/src/main/java/com/example/samplecmp/RegisterActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.samplecmp.databinding.ActivityRegisterBinding
 import com.example.samplecmp.databinding.ActivityTeAmoBinding
+import com.example.samplecmp.model.Usuario
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,12 +21,21 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.crearcuenta.setOnClickListener{
+            crearUsuario()
             startActivity(Intent(this, TeAmoActivity::class.java))
+
         }
 
         binding.yatengocuenta.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
 
         }
+    }
+
+    private fun crearUsuario() {
+        val nombre = binding.nombre.text.toString()
+        val correo = binding.correo.text.toString()
+
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/samplecmp/model/Usuario.kt
+++ app/src/main/java/com/example/samplecmp/model/Usuario.kt
@@ -1,4 +1,21 @@
 package com.example.samplecmp.model
 
 class Usuario {
+    var nombre: String = ""
+    var apellido: String = ""
+    var correo: String = ""
+    var contrasena: String = ""
+    var telefono: String = ""
+
+    constructor(nombre: String, apellido: String, correo: String, contrasena: String, telefono: String) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.correo = correo
+        this.contrasena = contrasena
+        this.telefono = telefono
+    }
+
+    override fun toString(): String {
+        return nombre
+    }
 }
